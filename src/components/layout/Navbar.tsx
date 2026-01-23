242726a0f5bf4fd5a6362b99cd06d3c62cab8f18
carla Miranda <waleilla99@gmail.com>
2026-01-23 11:40:25 -0300

    solo para aprender

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
-
+//cambios
 type CategoryCard = {
   title: string;
   subtitle: string;
